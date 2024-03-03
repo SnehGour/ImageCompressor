d3356934e979f18b0324fd6053b6096362f38451
snehgour80@gmail.com <snehgour80@gmail.com>
2024-03-03 22:23:15 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ImageCompressor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
