4092d96a5b5211d5e7f45678d74940cad89b8988
snehgour80@gmail.com <snehgour80@gmail.com>
2024-03-04 08:45:17 +0530

    Image Upload Service created

--- Services/BlobStorageService.cs
+++ Services/BlobStorageService.cs
@@ -10,26 +10,31 @@ namespace ImageCompressor.Services
         private readonly string _storageAccount;
         private readonly string _key;
         private readonly string _blobContainerName;
-        private readonly BlobServiceClient _blobServiceClient;
         private readonly IConfiguration _configuration;
+
+        private readonly BlobServiceClient _blobServiceClient;
+        private readonly BlobContainerClient _blobContainerClinet;
+
+
         public BlobStorageService(IConfiguration configuration)
         {
             _configuration = configuration;
-            var credential = new StorageSharedKeyCredential(_storageAccount,key)
-            string connectionString = _configuration["Azure:BlobStorageConnection"];
-            _blobServiceClient = new BlobServiceClient(connectionString);
+            _key = _configuration["Azure:BlobStorageKey"];
+            _storageAccount = _configuration["Azure:StorageAccountName"];
+            _blobContainerName = _configuration["Azure:ContainerName"];
+
+            var credential = new StorageSharedKeyCredential(_storageAccount, _key);
+            var blobUrl = $"https://{_storageAccount}.blob.core.windows.net";
+
+            _blobServiceClient = new BlobServiceClient(new Uri(blobUrl),credential);
+            _blobContainerClinet = _blobServiceClient.GetBlobContainerClient(_blobContainerName);
         }
         public async Task<string> UploadImageAsync(IFormFile file)
         {
             try
             {
-                //string connectionString = _configuration["Azure:BlobStorageConnection"];
-                
-                
-                _containerName = _configuration.GetConnectionString("simagecompressor");
                 string blobName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
-                BlobContainerClient containerClient = _blobServiceClient.GetBlobContainerClient(_containerName);
-                BlobClient blobClient = containerClient.GetBlobClient(blobName);
+                BlobClient blobClient= _blobContainerClinet.GetBlobClient(blobName);
                 await blobClient.UploadAsync(file.OpenReadStream(), true);
                 return blobClient.Uri.ToString();
 
